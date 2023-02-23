4082d6f3b09fc43f88ce0d80c4348d758712288a
Yoshiyuki Kitaguchi <kitaguchi@yk-gankeisei.com>
2023-02-23 11:00:09 +0900

    Initial Commit

---
+++ Grav_app_simple/Grav_app_simpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grav_app_simpleApp.swift
+//  Grav_app_simple
+//
+//  Created by Yoshiyuki Kitaguchi on 2023/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Grav_app_simpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
